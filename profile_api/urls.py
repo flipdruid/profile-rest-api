f19329d63885043130d2b8a96041e2b3b33c21a8
flipdruid <flipdruid@yahoo.com>
2023-01-08 15:58:19 +0800

    01082022

---
+++ profile_api/urls.py
@@ -0,0 +1,9 @@
+# from django.urls import path
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# app_name = "profile_api"
+
+# urlpatterns = [
+#     path ('', views.IndexView.as_view(), name='home'),
+# ]
